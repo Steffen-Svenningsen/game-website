f58eaa3fb232f186cf1571fa89cf7d168027e9c8
Steffen-Svenningsen <steffen@zynaps.dk>
2022-12-01 11:08:12 +0100

    Add layout file

---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import NavHeader from "@/components/NavHeader.vue";
+</script>
+
+<template>
+  <div class="layout">
+    <div class="layout-body">
+      <div class="layout-main">
+        <NavHeader />
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+@import ../assets/global
+.layout
+    height: 100%
+    background: $background
+
+    .layout-body
+        height: 100%
+        display: flex
+        align-items: center
+        justify-content: center
+
+        .layout-main
+            width: 100%
+            height: 100%
+            padding: 1rem
+</style>
